d1ba84d0fc3e2c0657f396ca1a401edd890f30f2
Mitchell <mitchinator1@gmail.com>
2017-05-31 14:32:07 -0700

    Update storage.js

--- storage.js
+++ storage.js
@@ -3,8 +3,8 @@ var entryX, entryY, grid;
 var Storage = {
     
     save: function () {
-        localStorage.setItem("grid.location", grid.location);
         localStorage.setItem("hud.heartCount", hud.heartCount);
+        localStorage.setItem("grid.location", grid.location);
         localStorage.setItem("grid.previousLocation", grid.previousLocation);
         console.log("Saved to localStorage");
     },
