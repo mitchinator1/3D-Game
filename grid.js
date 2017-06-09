799f94e5a9f1bc64f6c5aaf14ddda530350f0cc9
Mitchell <mitchinator1@gmail.com>
2017-06-09 16:23:41 -0700

    Create grid.js

--- grid.js
+++ grid.js
@@ -143,13 +143,8 @@ var Grid = {
     render: function () {
         'use strict';
         var i, j;
-        //camera.maxX = 0;
         
         for (i = 0; i < mapGrid[this.location][this.floor][this.gridY][this.gridX].length; i += 1) {
-                
-             //if (mapGrid[this.location][this.floor][this.gridY][this.gridX][i].length - 1) * blockW > camera.maxX) {
-                 //camera.maxX = (mapGrid[this.location][this.floor][this.gridY][this.gridX][i].length - 1) * blockW;
-             //}
             
             for (j = 0; j < mapGrid[this.location][this.floor][this.gridY][this.gridX][i].length; j += 1) {
                     
