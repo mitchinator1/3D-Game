8d7b293005ecf405cd6cd2ec3e655da6a8e7e523
Mitchell <mitchinator1@gmail.com>
2017-06-09 18:31:57 -0700

    Create storage.js

--- storage.js
+++ storage.js
@@ -3,18 +3,26 @@ var entryX, entryY, Grid, HUD;
 var Storage = {
     
     save: function () {
+        
         localStorage.setItem("HUD.heartCount", HUD.heartCount);
         localStorage.setItem("Grid.location", Grid.location);
         localStorage.setItem("Grid.previousLocation", Grid.previousLocation);
         localStorage.setItem("Grid.floor", Grid.floor);
         console.log("Saved to localStorage");
+        
     },
     
     load: function () {
+        
         HUD.heartCount = parseInt(localStorage.getItem("HUD.heartCount")) || 5;
         Grid.location = localStorage.getItem("Grid.location") || "Overworld";
         Grid.previousLocation = localStorage.getItem("Grid.previousLocation") || "";
         Grid.floor = parseInt(localStorage.getItem("Grid.floor")) || 0;
+        
+    },
+    
+    clear: function () {
+        localStorage.clear();
     }
     
 };
