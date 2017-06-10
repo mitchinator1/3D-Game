5c4fb77335b874ed0aa4623e85a498c41a24a701
Mitchell <mitchinator1@gmail.com>
2017-06-09 17:52:04 -0700

    Create main.js

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-var THREE, HUD, Player, Input, Grid, Camera, Lighting, scene = new THREE.Scene(), renderer = new THREE.WebGLRenderer( { antialias: true } ),
+var THREE, scene = new THREE.Scene(), renderer = new THREE.WebGLRenderer( { antialias: true } ),
     canvas = document.getElementById("hud"), ctx = canvas.getContext("2d"),
-    hud = Object.create(HUD), player = Object.create(Player), input = Object.create(Input), storage = Object.create(Storage), grid = Object.create(Grid), camera = Object.create(Camera), lighting = Object.create(Lighting),
     mainCam, pointLight, ambLight,
     planeGeo, planeMat, plane;
-
-//scene.fog = new THREE.Fog(0x000000, 0.015, 100);
-//scene.fog = new THREE.FogExp2( 0x000000, 0.045 );
 
 (function init() {
     'use strict';
@@ -14,17 +10,17 @@ var THREE, HUD, Player, Input, Grid, Camera, Lighting, scene = new THREE.Scene()
     renderer.setSize(900, 700);
     document.body.appendChild(renderer.domElement);
     
-    storage.load();
+    Storage.load();
     
-    player.init();
-    player.controllable = true;
+    Player.init();
+    Player.controllable = true;
     
-    camera.init("mainCam", 10, 15);
-    lighting.init(45);
-    hud.init();
-    input.init();
+    Camera.init("mainCam", 10, 15);
+    Lighting.init(45);
+    HUD.init();
+    Input.init();
     
-    grid.init();
+    Grid.init();
     
     plane = new THREE.Mesh(planeGeo, planeMat);
     plane.receiveShadow = true;
@@ -37,9 +33,9 @@ var render = function (ts) {
     'use strict';
     
     renderer.render(scene, mainCam);
-    hud.refresh();
+    HUD.refresh();
     
-    input.movement(ts, mainCam);
+    Input.movement(ts, mainCam);
     
     requestAnimationFrame(render);
     
