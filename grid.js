04e6c82899cd294e8c3b5722b7ed553d26442a0b
Mitchell <mitchinator1@gmail.com>
2017-06-16 10:58:32 -0700

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,7 +1,7 @@
 var blockW, blockD, blockH, blockGeos, blockMats,
     THREE, scene, Player, Camera, Lighting, ctx, canvas, HUD, Storage, mapGrid, mapGridTest;
 
-console.log("10:42");
+console.log("10:59");
 
 var Grid = {
     
@@ -379,9 +379,13 @@ var Grid = {
         
         if (block.set === "Passable") {
             switch (typeSrc) {
-                case 0:
-                    break;
-                default:
+                case 0: block.geometry = new THREE.BoxGeometry(blockW, blockD, 0.01);
+                    block.material = new THREE.MeshLambertMaterial({ color: 0x33aa00 });
+                    block.hitPad = 0;
+                    break;
+                default: block.geometry = new THREE.boxGeometry(blockW / 2, blockD, blockH * 5);
+                    block.material = new THREE.MeshLambertMaterial({ color: 0xffffff });
+                    block.hitPad = 1;
                     break;
                            }
         }
