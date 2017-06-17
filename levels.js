4515f2b6a8b6a22c90bee7c33e55daa478d3482a
Mitchell <mitchinator1@gmail.com>
2017-06-17 16:40:31 -0700

    Update levels.js

--- levels.js
+++ levels.js
@@ -12,37 +12,7 @@ var THREE, blockW, blockD, blockH;
 var sign1 = "You are now entering Dungeon 1.";
 var sign2 = "You are now entering Dungeon 2. Complete Dungeon 1 first.";
 var signs = [sign1, sign2];
-/*
-var blockGeo0 = new THREE.BoxGeometry(blockW, blockD, 0.01),
-    blockMat0 = new THREE.MeshLambertMaterial({ color: 0x33aa00 });
-var blockGeo1 = new THREE.BoxGeometry(blockW, blockD, blockH * 2),
-    blockMat1 = new THREE.MeshLambertMaterial({ color: 0xddaa55 });
-var blockGeo2 = new THREE.BoxGeometry(blockW, blockD, blockH * 2),
-    blockMat2 = new THREE.MeshLambertMaterial({ color: 0x119922 });
-var blockGeo3 = new THREE.BoxGeometry(blockW, blockD, 0.01),
-    blockMat3 = new THREE.MeshLambertMaterial({ color: 0x000000 });
-var blockGeo4 = new THREE.BoxGeometry(blockW, blockD, 0.01),
-    blockMat4 = new THREE.MeshLambertMaterial({ color: 0x000000 });
-var blockGeo6 = new THREE.BoxGeometry(blockW, blockD, blockH / 2),
-    blockMat6 = new THREE.MeshLambertMaterial({ color: 0xdddd55 });
-var blockGeo7 = new THREE.BoxGeometry(blockW, blockD, blockH),
-    blockMat7 = new THREE.MeshLambertMaterial({ color: 0xddaa00 });
 
-var b = {bt: '', hit: false}, //blank   
-    o = {bt: 0, hit: false, hitPad: 0, geometry: blockGeo0, material: blockMat0 }, //space
-    w = {bt: 1, hit: true, hitPad: 0.9, geometry: blockGeo1, material: blockMat1 }, //wall
-    s = {bt: 2, hit: false, hitPad: 0, geometry: blockGeo2, material: blockMat2 }, //shrub
-    d1 = {bt: 3, hit: true, hitPad: 0.1, geometry: blockGeo3, material: blockMat3 }, //Dungeon 1 door
-    d2 = {bt: 4, hit: true, hitPad: 0.1, geometry: blockGeo4, material: blockMat4 }, //Dungeon 2 door
-    id1 = {bt: 5, hit: true, hitPad: 0, geometry: blockGeo4, material: blockMat4 }, //Inner dungeon door
-    id2 = {bt: 8, hit: true, hitPad: 0.2, locked: true, geometry: new THREE.BoxGeometry(blockW / 4, blockD, blockH * 2), material: blockMat1 }, //Inner locked dungeon door
-    c1 = {bt: 6, hit: true, hitPad: 1, interact: {item: "Heart", coins: 5, opened: false}, geometry: blockGeo6, material: blockMat6  }; //Chest
-
-var sign = function (index) {
-    'use strict';
-    return {bt: 7, hit: true, hitPad: 1, contents: signs[index], geometry: blockGeo7, material: blockMat7 };
-};
-*/
 // dungeon number, floor, x coordinate, y coordinate
 
 var overworld000 = [
@@ -81,66 +51,67 @@ var dungeon1010 = [
     [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
     [16384, 16384, 16384, 16384, 16384, 49153, 16384, 16384, 16384, 16384, 16384]
 ];
-/*
+
 var dungeon1020 = [
-    [w, w, w, w, w, id1, w, w, w, w, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [id1, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, w, w, w, w, w, w, w, w, w, w]
+    [16384, 16384, 16384, 16384, 16384, 49152, 16384, 16384, 16384, 16384, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [49152, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384]
 ];
 
 var dungeon1001 = [
-    [w, w, w, w, w, w, w, w, w, w, w, w, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, o, o, id1],
-    [w, o, o, o, o, o, o, o, o, o, o, o, w],
-    [w, w, w, w, w, w, w, w, w, w, w, w, w]
+    [16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 512, 0, 512, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 49152],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384]
 ];
 
 var dungeon1011 = [
-    [w, w, w, w, w, w, w, w, w, w, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [id1, o, o, o, o, o, o, o, o, o, id1],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, w, w, w, w, w, w, w, w, w, w]
+    [16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [49152, 0, 0, 0, 0, 0, 0, 0, 0, 0, 49152],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384]
+
 ];
 
 var dungeon1021 = [
-    [w, w, w, w, w, w, w, w, w, w, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [id1, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, o, o, o, o, o, o, o, o, o, w],
-    [w, w, w, w, w, id1, w, w, w, w, w]
+    [16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [49152, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16384],
+    [16384, 16384, 16384, 16384, 16384, 49152, 16384, 16384, 16384, 16384, 16384]
 ];
-     
+/*
 var dungeon2000 = [
     [b, b, b, b, b, w, w, w],
     [b, b, b, b, w, o, o, w],
