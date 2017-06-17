83f95b2281149f751d8620215219b6348b4f7a4f
Mitchell <mitchinator1@gmail.com>
2017-06-17 14:27:08 -0700

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,7 +1,7 @@
 var blockW, blockD, blockH, blockGeos, blockMats,
     THREE, scene, Player, Camera, Lighting, ctx, canvas, HUD, Storage, mapGrid, mapGridTest;
 
-console.log("2:22");
+console.log("2:28");
 
 var Grid = {
     
